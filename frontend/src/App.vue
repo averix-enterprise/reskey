67e2b69813f4df37b49408851d39bc733dff6977
ReaperMaga <reapermaga@gmail.com>
2023-11-13 10:13:01 +0100

    :fire: Init

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import {WindowSetSize} from "../wailsjs/runtime/runtime"
+
+function test() {
+  WindowSetSize(500, 250)
+}
+</script>
+
+<template>
+  <button class="bg-blue-900"  @click="test()">Click me bitch</button>
+</template>
+
+
