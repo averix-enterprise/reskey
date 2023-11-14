7333fc00f4db8b6d091352e2f772ac93aa69fe53
Zoey <zoeykaiser8@gmail.com>
2023-11-14 14:42:14 +0100

    feat: Added mock frontend (#5)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,39 @@
 <script lang="ts" setup>
-import {WindowSetSize} from "../wailsjs/runtime/runtime"
+import { WindowSetSize } from "../wailsjs/runtime/runtime";
+import Row from "./components/HotKey/HotKeyRow.vue"
+import { ref, watch } from "vue";
 
-function test() {
-  WindowSetSize(500, 250)
+const rowAmount = ref(1)
+
+const addRow = () => {
+  rowAmount.value++
 }
+
+watch(rowAmount, (newValue) => {
+  const baseHeight = 250
+  const windowHeight = baseHeight + (rowAmount.value * 50 )
+  if (windowHeight <= 800) {
+      WindowSetSize(600, windowHeight)
+  }
+})
 </script>
 
 <template>
-  <button class="bg-blue-900"  @click="test()">Click me bitch</button>
+  <div class="p-4">
+    <div class="flex items-center justify-between">
+      <div class="flex items-center gap-1">
+        <h1 class="text-lg font-bold text-center">
+          Res<span class="text-purple-800">Key</span>
+        </h1>
+        <button @click="addRow">+</button>
+      </div>
+      <div>
+        <button @click="rowAmount = 1">Refresh</button>
+      </div>
+    </div>
+    <hr class="my-3" />
+    <div class="flex flex-col gap-2">
+      <Row v-for="row in rowAmount" />
+    </div>
+  </div>
 </template>
-
-
---
+++ frontend/src/components/HotKey/HotKeyRow.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-gray-200 p-2 rounded">
+        <div class="flex items-center gap-2 justify-between">
+          Hotkey:
+          <input class="p-1 text-sm rounded" placeholder="Alt + SHIFT" />
+        </div>
+        <div>
+          <input class="bg-transparent text-2xl" placeholder="1920 x 1080" />
+        </div>
+    </div>
+</template>
